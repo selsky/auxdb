dd82e9ad8fef3f1a8f69907377ebfbbdbbacb09f
Eric E Moore <eemoore@fyndo.com>
2013-02-08 01:52:03 -0500

    formatting, add link to aps11

--- personnel/urls.py
+++ personnel/urls.py
@@ -9,9 +9,12 @@ from django.views.generic import DetailView
 
 urlpatterns = patterns('', 
                        url(r'^$', ListView.as_view(model=Person)), 
-                       url(r'^person/(?P<pk>-?\d+)$', DetailView.as_view(model=Person)), 
+                       url(r'^person/(?P<pk>-?\d+)$', 
+                           DetailView.as_view(model=Person)), 
                        url(r'^person/(?P<pk>-?\d+)/aps1.pdf$', aps1_pdf), 
-                       url(r'^person/(?P<pk>-?\d+)/(?P<fy>\d\d\d\d)/aps11.html$', aps11_html), 
-                       url(r'^ten/(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2}).html$', aps10_html, name='aps10_html'), 
+                       url(r'^person/(?P<pk>-?\d+)/(?P<fy>\d{4})/aps11.html$', 
+                           aps11_html), 
+                       url(r'^ten/(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2}).html$', 
+                           aps10_html, name='aps10_html'), 
                        )
                        
